69811117e1a350d6b4f2031a46dcbfab5b0d24c7
meg4ik <meg4mag@gmail.com>
2021-02-03 13:55:04 +0200

    add pages app

--- django_second_project/products/models.py
+++ django_second_project/products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(default='this is cool!')
+    featured = models.BooleanField(default=False)
