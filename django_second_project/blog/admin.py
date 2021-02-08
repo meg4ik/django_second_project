46d5bef368b82b5c5d9b8a0893db2adeb84e62cb
meg4ik <meg4mag@gmail.com>
2021-02-08 15:09:38 +0200

    fix Article model permission to admin

--- django_second_project/blog/admin.py
+++ django_second_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Article
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Article, AuthorAdmin)
