eec37d84f7ce806c7503ed57a7201ff0d217bc98
meg4ik <meg4mag@gmail.com>
2021-02-10 10:57:00 +0200

    add str method

--- django_second_project/products/models.py
+++ django_second_project/products/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(default='this is cool!')
     featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
